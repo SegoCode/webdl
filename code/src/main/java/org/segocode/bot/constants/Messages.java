e108ef307ebfdec7ea98bf05a733c4c2c72d12ca
SegoCode <35817798+SegoCode@users.noreply.github.com>
2025-03-25 16:59:45 +0100

    Update Messages.java

--- code/src/main/java/org/segocode/bot/constants/Messages.java
+++ code/src/main/java/org/segocode/bot/constants/Messages.java
@@ -4,5 +4,5 @@ public class Messages {
     public static final String NOT_VALID_LINK = "❌ That doesn't seem to be a valid link";
     public static final String DOWNLOAD_REQUEST = "🔄 Download request processing...";
     public static final String DOWNLOAD_REQUEST_QUEUED = "⌛ Your request is in queue...";
-    public static final String DOWNLOAD_REQUEST_ERROR = "❌ An error occurred while processing your request: web not supported, private profile or reach limit.";
+    public static final String DOWNLOAD_REQUEST_ERROR = "❌ Unable to download: web not supported, private post or reach limit";
 }
